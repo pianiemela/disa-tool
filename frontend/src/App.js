4dbe26c0c9695063fd26be12ae603393f2878672
Mikko Tianen <mikko.masala@gmail.com>
2018-06-22 16:22:59 +0300

    basic frontend boilerplate

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Button } from 'semantic-ui-react'
+
+class App extends Component {
+  render() {
+    console.log('hello kelo')
+    return (
+      <div>
+        <h1>Hello hello</h1>
+        <Button color="blue">button</Button>
+      </div>
+    )
+  }
+}
+
+export default App
