15ccb6f6d69831d30a1d80dd355279a0527c856e
Salo Jemi S <jemi.salo@gmail.com>
2018-07-02 18:37:58 +0300

    Add number input to tasks.

---
+++ frontend/src/containers/Course/components/tasks/SkillSlider.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { Grid, Input } from 'semantic-ui-react'
+import './tasks.css'
+
+class SkillSlider extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: 0.8
+    }
+  }
+
+  changeValue = e => {
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  render() {
+    return (
+      <Grid.Row className="skillSlider">
+        <Grid.Column textAlign="right">
+          <h3>{this.props.skill}</h3>
+          <Input className="numberInput" type="number" min={0} max={1} step={0.01} value={this.state.value} onChange={this.changeValue} />
+          <Input className="RangeInput" type="range" min={0} max={1} step={0.01} value={this.state.value} onChange={this.changeValue} />
+        </Grid.Column>
+      </Grid.Row>
+    )
+  }
+}
+
+export default SkillSlider
--- frontend/src/containers/Course/components/tasks/Task.js
+++ frontend/src/containers/Course/components/tasks/Task.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Grid, Button } from 'semantic-ui-react'
 import './tasks.css'
+import SkillSlider from './SkillSlider';
 
 class Task extends Component {
   constructor(props) {
@@ -30,12 +31,7 @@ class Task extends Component {
           </Grid.Column>
         </Grid.Row>
           {this.props.taskSkills.map(skill => (
-            <Grid.Row key={skill}>
-              <Grid.Column textAlign="right">
-                <h3>{skill}</h3>
-                <input type="range" min={0} max={1} step={0.01} defaultValue={0.8} />
-              </Grid.Column>
-            </Grid.Row>
+            <SkillSlider key={skill} skill={skill} />
           ))}
       </Grid>
     )
@@ -44,7 +40,7 @@ class Task extends Component {
   render() {
     return (
       <div className="task">
-        <Button onClick={this.toggleExpanded} basic={!this.state.expanded} color="green" fluid>{this.props.task.name}</Button>
+        <Button onClick={this.toggleExpanded} basic={!this.state.expanded} fluid>{this.props.task.name}</Button>
         {this.renderExpanded()}
       </div>
     )
