4c95c5347884160baa01b328152801f48e51d226
Mikko Tiainen <mikko.o.tiainen@helsinki.fi>
2018-07-03 16:30:27 +0300

    some category seeds

--- backend/src/database/create_data.js
+++ backend/src/database/create_data.js
@@ -1,11 +1,31 @@
 const { sequelize } = require('./connection.js')
-const { Task } = require('./models.js')
+const categories = require('./seeds/categories.json')
+const { Task,
+    TaskType,
+    Category,
+    Objective,
+    TaskObjective,
+    SkillLevel,
+    Grade,
+    Course,
+    CourseInstance,
+    Person,
+    PersonRole,
+    CoursePerson,
+    SelfAssessment,
+    TaskResponse,
+    AssessmentResponse } = require('./models.js')
 
+
+const createCategories = () => {
+    Category.bulkCreate(categories)
+}
 
 const run = async () => {
     await sequelize.sync({ force: true })
     console.log('forced')
-    Task.create()
+    createCategories()
+    return null
 }
 
 run()
