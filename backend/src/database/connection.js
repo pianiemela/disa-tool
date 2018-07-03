1e0908b5838b036ab241095a0087a3410064de03
Mikko Tiainen <mikko.o.tiainen@helsinki.fi>
2018-07-03 14:28:19 +0300

    created database structure

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -2,10 +2,10 @@ const Sequelize = require('sequelize')
 const conf = require('../../conf-backend.js')
 
 const sequelize = new Sequelize(conf.DB_URL, {
-  dialect: 'postgres',
-  logging: false
+    dialect: 'postgres',
+    logging: false
 })
 
 module.exports = {
-  sequelize
+    sequelize
 }
