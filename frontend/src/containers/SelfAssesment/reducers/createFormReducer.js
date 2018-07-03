f2f542ba82e37dcf3071075cf52aceb8fadc968c
laimikko1 <mikko.s.laitinen@helsinki.fi>
2018-07-03 19:31:04 +0300

    Set up reducer and update service

---
+++ frontend/src/containers/SelfAssesment/reducers/createFormReducer.js
@@ -0,0 +1,19 @@
+const INITIAL_STATE = {
+    categoryFormStructure: {},
+    objectiveFormStructure: {},
+    formData: {}
+}
+
+
+const createFormReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case 'GET_SELF_ASSESMENT_DATA':
+        console.log(action.apiresponse)
+            const { selfAssesmentData } = action.apiresponse.data
+            return {...state, formData: selfAssesmentData }
+        default:
+            return state
+    }
+}
+
+export default createFormReducer
