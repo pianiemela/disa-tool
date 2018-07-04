ebc71d9987d2bf5d1f709517a78ae7b9b513d80d
Mikko Tiainen <mikko.o.tiainen@helsinki.fi>
2018-07-04 18:43:44 +0300

    started course endpoint

--- backend/src/controllers/category_controller.js
+++ backend/src/controllers/category_controller.js
@@ -1,19 +1,10 @@
 const router = require('express').Router()
+const { validateLang } = require('../middleware/validate.js')
 
 const categoryService = require('../services/category_service.js')
 
-const isLangCorrect = (lang) => {
-    if (!lang) return false
-    if (lang !== 'eng' && lang !== 'fin' && lang !== 'swe') return false
-    return true
-}
-
-router.get('/', async (req, res) => {
+router.get('/', validateLang, async (req, res) => {
     const { lang, courseInstanceId } = req.query
-    if (!isLangCorrect(lang)) {
-        res.status(400).json({ error: 'Language not correctly specified' })
-        return null
-    }
     const categories = await categoryService.getCourseCategories(courseInstanceId, lang)
     res.status(200).json(categories)
 })
