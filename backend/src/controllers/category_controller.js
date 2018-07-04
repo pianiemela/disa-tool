bdabd6a2ef2519d1b53dc17e7130d80a9e05caad
Mikko Tiainen <mikko.o.tiainen@helsinki.fi>
2018-07-04 15:02:06 +0300

    category fetching with language

--- backend/src/controllers/category_controller.js
+++ backend/src/controllers/category_controller.js
@@ -2,13 +2,19 @@ const router = require('express').Router()
 
 const categoryService = require('../services/category_service.js')
 
+const isLangCorrect = (lang) => {
+    if (!lang) return false
+    if (lang !== 'eng' && lang !== 'fin' && lang !== 'swe') return false
+    return true
+}
+
 router.get('/', async (req, res) => {
-    const categories = []
-    if (req.query) {
-        categories.push(await categoryService.getCourseCategories(req.query.course))
-    } else {
-        categories.push(await categoryService.getAllCategories())
+    const { lang, courseInstanceId } = req.query
+    if (!isLangCorrect(lang)) {
+        res.status(400).json({ error: 'Language not correctly specified' })
+        return null
     }
+    const categories = await categoryService.getCourseCategories(courseInstanceId, lang)
     res.status(200).json(categories)
 })
 
