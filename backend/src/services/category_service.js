1e96a930bc11641f1ad2ceb7b2e5f5bfc2b4d265
Mikko Tiainen <mikko.o.tiainen@helsinki.fi>
2018-07-04 13:32:52 +0300

    end point to serve categories by course instance id

--- backend/src/services/category_service.js
+++ backend/src/services/category_service.js
@@ -1,7 +1,12 @@
-const { Category } = require('../database/models.js')
+const { Category, Objective } = require('../database/models.js')
 
 const getAllCategories = () => Category.findAll()
 
+const getCourseCategories = courseInstanceId => (
+    Category.findAll({ include: { model: Objective, where: { course_instance_id: courseInstanceId } } })
+)
+
 module.exports = {
-    getAllCategories
+    getAllCategories,
+    getCourseCategories
 }
