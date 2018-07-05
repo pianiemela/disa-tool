466e269c493d1aa92d283458906642e65e315c87
Mikko Tiainen <mikko.o.tiainen@helsinki.fi>
2018-07-05 14:48:04 +0300

    created user page

--- frontend/src/containers/Main/main.js
+++ frontend/src/containers/Main/main.js
@@ -4,10 +4,12 @@ import { withRouter, Switch, Route } from 'react-router-dom'
 import LoginPage from '../Login/LoginPage'
 import CoursePage from '../Course/CoursePage'
 import SelfAssesmentPage from '../SelfAssesment/SelfAssesmentPage'
+import UserPage from '../User/UserPage'
 
 const Main = () => (
   <main>
     <Switch>
+      <Route exact path="/user" component={UserPage} />
       <Route exact path="/course" render={() => <CoursePage courseId={1} />} />
       <Route exact path="/selfAssesment" component={SelfAssesmentPage} />
       <Route component={LoginPage} />
--- frontend/src/containers/Nav/navbar.js
+++ frontend/src/containers/Nav/navbar.js
@@ -1,23 +1,61 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { withRouter, Link } from 'react-router-dom'
 import { Menu } from 'semantic-ui-react'
 
-const Nav = () => (
-  <nav>
-    <Menu stackable inverted>
-      <Menu.Menu position="left">
-        <Menu.Item header>
-          DISA-työkalu
-        </Menu.Item>
-        <Menu.Item name="Course" as={Link} to="/course">
-          Kurssi
-        </Menu.Item>
-        <Menu.Item name="SelfAssesment" as={Link} to="/selfAssesment">
-          Itsearvio
-        </Menu.Item>
-      </Menu.Menu>
-    </Menu>
-  </nav>
-)
+class Nav extends Component {
+  state = { activeItem: 'home' }
+
+  handleClick = (e, { name }) => {
+    this.setState({ activeItem: name })
+  }
+
+  render() {
+    return (
+      <nav>
+        <Menu tabular>
+          <Menu.Menu position="left">
+            <Menu.Item
+              header
+              as={Link}
+              to="/"
+              name="home"
+              active={this.state.activeItem === 'home'}
+              onClick={this.handleClick}
+            >
+              DISA-työkalu
+            </Menu.Item>
+            <Menu.Item
+              as={Link}
+              to="/user"
+              name="user"
+              active={this.state.activeItem === 'user'}
+              onClick={this.handleClick}
+            >
+              Oma sivu
+            </Menu.Item>
+            <Menu.Item
+              as={Link}
+              to="/course"
+              name="course"
+              active={this.state.activeItem === 'course'}
+              onClick={this.handleClick}
+            >
+              Kurssi
+            </Menu.Item>
+            <Menu.Item
+              as={Link}
+              to="/selfAssesment"
+              name="assessment"
+              active={this.state.activeItem === 'assessment'}
+              onClick={this.handleClick}
+            >
+              Itsearvio
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
+      </nav>
+    )
+  }
+}
 
 export default withRouter(Nav)
