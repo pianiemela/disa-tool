b957f794750ef4d752db37ce6b3e3d4d3841210c
Mikko Tiainen <mikko.o.tiainen@helsinki.fi>
2018-07-06 14:23:37 +0300

    outlining user page

--- frontend/src/redux/user.js
+++ frontend/src/redux/user.js
@@ -1,8 +1,11 @@
 
 export const userReducer = (state = [], action) => {
+  console.log(action)
   switch (action.type) {
-    case 'GET_USER':
+    case 'GET_USER_SUCCESS':
       return action.payload
+    case 'GET_USER_FAILURE':
+      return state
     default:
       return state
   }
