0305d55c37cab765d0cc41e0903ca5bedbf80b18
Salo Jemi S <jemi.salo@gmail.com>
2018-07-09 15:21:14 +0300

    Test CoursePage renders children.

--- frontend/src/tests/containers/Course/CoursePage.test.js
+++ frontend/src/tests/containers/Course/CoursePage.test.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import { CoursePage } from '../../../containers/Course/CoursePage'
+import CourseHeader from '../../../containers/Course/components/header/CourseHeader'
+import Matrix from '../../../containers/Course/components/matrix/Matrix'
+import Tasklist from '../../../containers/Course/components/tasks/Tasklist'
+import Typelist from '../../../containers/Course/components/types/Typelist'
 
 describe('Course page', () => {
   let wrapper
@@ -17,5 +21,21 @@ describe('Course page', () => {
 
   it('renders', () => {
     expect(wrapper.find('.CoursePage').exists()).toEqual(true)
+  })
+
+  it('renders CourseHeader', () => {
+    expect(wrapper.find(CourseHeader).exists()).toEqual(true)
+  })
+
+  it('renders Matrix', () => {
+    expect(wrapper.find(Matrix).exists()).toEqual(true)
+  })
+
+  it('renders Tasklist', () => {
+    expect(wrapper.find(Tasklist).exists()).toEqual(true)
+  })
+
+  it('renders Typelist', () => {
+    expect(wrapper.find(Typelist).exists()).toEqual(true)
   })
 })
