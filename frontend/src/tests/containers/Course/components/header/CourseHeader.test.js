7124c732ab87500561cfaa95ccb8ed307db39856
Salo Jemi S <jemi.salo@gmail.com>
2018-07-09 15:52:32 +0300

    Test CourseHeader renders course name.

--- frontend/src/tests/containers/Course/components/header/CourseHeader.test.js
+++ frontend/src/tests/containers/Course/components/header/CourseHeader.test.js
@@ -2,11 +2,21 @@ import React from 'react'
 import { CourseHeader } from '../../../../../containers/Course/components/header/CourseHeader'
 
 const mockFn = () => {}
+const courseName = 'Test Course'
 const createWrapper = () => shallow(<CourseHeader
-  courseName="Test Course"
+  courseName={courseName}
   editing={false}
   setEditing={mockFn}
 />)
+
+const findText = (text, wrapper) => {
+  let found = 0
+  if (wrapper.text().includes(text)) found += 1
+  wrapper.children().forEach((child) => {
+    found += findText(text, child)
+  })
+  return found
+}
 
 describe('CourseHeader component', () => {
   let wrapper
@@ -17,5 +27,9 @@ describe('CourseHeader component', () => {
 
   it('renders', () => {
     expect(wrapper.find('.CourseHeader').exists()).toEqual(true)
+  })
+
+  it('renders course name', () => {
+    expect(findText(courseName, wrapper.find('.headerBlock'))).toEqual(1)
   })
 })
