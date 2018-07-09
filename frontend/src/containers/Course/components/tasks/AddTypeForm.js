d38ab4c22568c1a2619d41b58eaa8571ef0d6cb9
Salo Jemi S <jemi.salo@gmail.com>
2018-07-09 11:58:04 +0300

    Fix bug that caused modals to open when clicking next to trigger.

--- frontend/src/containers/Course/components/tasks/AddTypeForm.js
+++ frontend/src/containers/Course/components/tasks/AddTypeForm.js
@@ -17,6 +17,10 @@ class AddTypeForm extends Component {
     }
   }
 
+  shouldComponentUpdate(newProps, newState) {
+    return newState.options.length > 0
+  }
+
   prepareOptions = () => {
     let options = []
     const excluded = {}
