7553738f3209972d1f504886ee44bf242b6ba574
Salo Jemi S <jemi.salo@gmail.com>
2018-07-09 16:28:06 +0300

    Test CourseHeader edit button functionality.

--- frontend/src/tests/containers/Course/components/header/CourseHeader.test.js
+++ frontend/src/tests/containers/Course/components/header/CourseHeader.test.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react'
 import { CourseHeader } from '../../../../../containers/Course/components/header/CourseHeader'
 import { findText } from '../../../../testUtils'
 
-const mockFn = () => {}
+let setEditing
 const courseName = 'Test Course'
-const createWrapper = () => shallow(<CourseHeader
-  courseName={courseName}
-  editing={false}
-  setEditing={mockFn}
-/>)
+const createWrapper = () => {
+  setEditing = jest.fn()
+  return shallow(<CourseHeader
+    courseName={courseName}
+    editing={false}
+    setEditing={setEditing}
+  />)
+}
 
 describe('CourseHeader component', () => {
   let wrapper
@@ -21,7 +25,38 @@ describe('CourseHeader component', () => {
     expect(wrapper.find('.CourseHeader').exists()).toEqual(true)
   })
 
-  it('renders course name', () => {
-    expect(findText(courseName, wrapper.find('.headerBlock'))).toEqual(1)
+  it('renders course name.', () => {
+    const headerBlock = wrapper.find('.headerBlock')
+    expect(findText(courseName, headerBlock)).toBeGreaterThan(0)
+  })
+
+  describe('edit button', () => {
+    it('renders a button.', () => {
+      const editButton = wrapper.find('.editBlock').find(Button)
+      expect(editButton.exists()).toEqual(true)
+    })
+
+    describe('while not editing', () => {
+      it('calls props.setEditing with correct value when edit button is clicked.', () => {
+        const editButton = wrapper.find('.editBlock').find(Button)
+        editButton.props().onClick()
+        expect(setEditing).toHaveBeenCalledWith({ value: true })
+      })
+    })
+
+    describe('while editing', () => {
+      beforeEach(() => {
+        wrapper.setProps({
+          ...wrapper.props(),
+          editing: true
+        })
+      })
+
+      it('calls props.setEditing with correct value when edit button is clicked.', () => {
+        const editButton = wrapper.find('.editBlock').find(Button)
+        editButton.props().onClick()
+        expect(setEditing).toHaveBeenCalledWith({ value: false })
+      })
+    })
   })
 })
