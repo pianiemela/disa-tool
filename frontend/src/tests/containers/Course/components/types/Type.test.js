4a505a1ecd09ed6fe570374c1b66bef835f8da8a
Salo Jemi S <jemi.salo@gmail.com>
2018-07-10 17:11:56 +0300

    Test Type inputs have correct value and call function prop on change.

--- frontend/src/tests/containers/Course/components/types/Type.test.js
+++ frontend/src/tests/containers/Course/components/types/Type.test.js
@@ -3,7 +3,8 @@ import { Type } from '../../../../../containers/Course/components/types/Type'
 
 const type = {
   id: 1,
-  name: 'Test Type'
+  name: 'Test Type',
+  multiplier: 0.5
 }
 
 describe('Type component', () => {
@@ -21,5 +22,53 @@ describe('Type component', () => {
 
   it('renders.', () => {
     expect(wrapper.find('.Type').exists()).toEqual(true)
+  })
+
+  describe('number input', () => {
+    let numberInput
+
+    beforeEach(() => {
+      numberInput = wrapper.find('.numberInput')
+    })
+
+    it('renders with the correct value.', () => {
+      expect(numberInput.props().value).toEqual(type.multiplier)
+    })
+
+    it('calls changeTypeMultiplier when value is changed.', () => {
+      numberInput.simulate('change', {
+        target: {
+          value: 0.4
+        }
+      })
+      expect(changeTypeMultiplier).toHaveBeenCalledWith({
+        id: type.id,
+        multiplier: 0.4
+      })
+    })
+  })
+
+  describe('range input', () => {
+    let rangeInput
+
+    beforeEach(() => {
+      rangeInput = wrapper.find('.rangeInput')
+    })
+
+    it('renders with the correct value.', () => {
+      expect(rangeInput.props().value).toEqual(type.multiplier)
+    })
+
+    it('calls changeTypeMultiplier when value is changed.', () => {
+      rangeInput.simulate('change', {
+        target: {
+          value: 0.4
+        }
+      })
+      expect(changeTypeMultiplier).toHaveBeenCalledWith({
+        id: type.id,
+        multiplier: 0.4
+      })
+    })
   })
 })
