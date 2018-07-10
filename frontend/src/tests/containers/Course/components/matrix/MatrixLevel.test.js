3426429ad7038c4fa20e6cd2f03d59210748ee04
Salo Jemi S <jemi.salo@gmail.com>
2018-07-10 11:41:49 +0300

    Test MatrixLevel renders a MatrixObjective for each objectuve.

--- frontend/src/tests/containers/Course/components/matrix/MatrixLevel.test.js
+++ frontend/src/tests/containers/Course/components/matrix/MatrixLevel.test.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import MatrixLevel from '../../../../../containers/Course/components/matrix/MatrixLevel'
+import MatrixObjective from '../../../../../containers/Course/components/matrix/MatrixObjective'
 
 const level = {
   id: 1,
   name: 'Test Level',
-  objectives: []
+  objectives: [
+    {
+      id: 1,
+      name: '1'
+    },
+    {
+      id: 2,
+      name: '2'
+    },
+    {
+      id: 3,
+      name: '3'
+    }
+  ]
 }
 
 describe('MatrixLevel component', () => {
@@ -21,5 +35,9 @@ describe('MatrixLevel component', () => {
 
   it('renders.', () => {
     expect(wrapper.find('.MatrixLevel').exists()).toEqual(true)
+  })
+
+  it('renders a MatrixObjective component for each objective.', () => {
+    expect(wrapper.find(MatrixObjective).length).toEqual(level.objectives.length)
   })
 })
