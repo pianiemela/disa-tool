f18a352f04a1d1d732172bfbbfea9eba4bf9a9d0
Salo Jemi S <jemi.salo@gmail.com>
2018-07-10 14:40:25 +0300

    Test Tasklist renders.

---
+++ frontend/src/tests/containers/Course/components/tasks/Tasklist.test.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Tasklist } from '../../../../../containers/Course/components/tasks/Tasklist'
+
+const tasks = [
+  {
+    id: 1,
+    name: 'Tehtävä 1. (Tee voltti)',
+    description: 'Tässä tehtävässä sinun tulee tehdä voltti, eli hypätä ja pyörähtää täysi kierros sivuttaisakselisi ympäri.',
+    max_points: 3,
+    info: 'tehtäväsivu: https://fi.wikipedia.org/wiki/Voltti_(akrobatia)',
+    objectives: [],
+    types: []
+  },
+  {
+    id: 2,
+    name: 'Tehtävä 2. (Tee Muuta)',
+    description: 'Tässä tehtävässä sinun tulee tehdä jotain muuta.',
+    max_points: 1,
+    info: 'käytä mielikuvitusta',
+    objectives: [],
+    types: []
+  }
+]
+
+describe('Tasklist component', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallow(<Tasklist
+      tasks={tasks}
+      courseId={1}
+      editing={false}
+    />)
+  })
+
+  it('renders.', () => {
+    expect(wrapper.find('.Tasklist').exists()).toEqual(true)
+  })
+})
