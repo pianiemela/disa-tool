8901ce2828aa9171497a7fdcfeeba41f0e6a7e5f
Salo Jemi S <jemi.salo@gmail.com>
2018-07-10 12:09:25 +0300

    Test MatrixObjective shows/hides RemoveObjectiveForm based on editing property.

--- frontend/src/tests/containers/Course/components/matrix/MatrixObjective.test.js
+++ frontend/src/tests/containers/Course/components/matrix/MatrixObjective.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import MatrixObjective from '../../../../../containers/Course/components/matrix/MatrixObjective'
+import RemoveObjectiveForm from '../../../../../containers/Course/components/matrix/RemoveObjectiveForm'
 import { findText } from '../../../../testUtils'
 
 const objective = {
@@ -23,5 +24,24 @@ describe('MatrixObjective component', () => {
 
   it('renders objective name', () => {
     expect(findText(objective.name, wrapper)).toBeGreaterThan(0)
+  })
+
+  describe('when not editing', () => {
+    it('does not render a RemoveObjectiveForm component.', () => {
+      expect(wrapper.find(RemoveObjectiveForm).exists()).toEqual(false)
+    })
+  })
+
+  describe('when editing', () => {
+    beforeEach(() => {
+      wrapper.setProps({
+        ...wrapper.props(),
+        editing: true
+      })
+    })
+
+    it('renders a RemoveObjectiveForm component.', () => {
+      expect(wrapper.find(RemoveObjectiveForm).exists()).toEqual(true)
+    })
   })
 })
