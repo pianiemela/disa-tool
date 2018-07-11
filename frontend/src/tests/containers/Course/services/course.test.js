24e060db17965a06c099bd9f5aba487004d604b3
Salo Jemi S <jemi.salo@gmail.com>
2018-07-11 12:00:19 +0300

    Test getCourseData resolves with the correct action.

--- frontend/src/tests/containers/Course/services/course.test.js
+++ frontend/src/tests/containers/Course/services/course.test.js
@@ -1,4 +1,12 @@
-import { getCourseData } from '../../../../containers/Course/services/course'
+import {
+  getCourseData,
+  hardCodedCategories,
+  hardCodedTypes,
+  hardCodedTasks,
+  hardCodedLevels,
+  hardCodedCourse
+} from '../../../../containers/Course/services/course'
+import asyncAction from '../../../../utils/asyncAction'
 
 describe('getCourseData function', () => {
   let data
@@ -11,5 +19,35 @@ describe('getCourseData function', () => {
 
   it('returns a promise.', () => {
     expect(typeof getCourseData(data).then).toEqual('function')
+  })
+
+  describe('dispatch', () => {
+    let withDispatch
+    let dispatch
+
+    beforeEach(() => {
+      dispatch = jest.fn()
+      withDispatch = asyncAction(getCourseData, dispatch)
+    })
+
+    it('is called with correct values.', async () => {
+      await withDispatch(data)
+      expect(dispatch).toHaveBeenCalledWith({
+        type: 'COURSE_GET_DATA',
+        response: {
+          data: {
+            course: {
+              ...hardCodedCourse,
+              id: data.courseId
+            },
+            tasks: hardCodedTasks,
+            categories: hardCodedCategories,
+            types: hardCodedTypes,
+            levels: hardCodedLevels
+          },
+          message: '<getDataForCourseSuccess>'
+        }
+      })
+    })
   })
 })
