1187f8880ce36145cdc9a8e03ee0a15ecfd5c030
Salo Jemi S <jemi.salo@gmail.com>
2018-07-11 12:17:41 +0300

    Test addObjective and removeObjective resolve with correct actions.

--- frontend/src/tests/containers/Course/services/objectives.test.js
+++ frontend/src/tests/containers/Course/services/objectives.test.js
@@ -1,6 +1,19 @@
 import { addObjective, removeObjective } from '../../../../containers/Course/services/objectives'
+import asyncAction from '../../../../utils/asyncAction'
 
 describe('addObjective function', () => {
+  const mockResponse = {
+    message: '<addCObjectiveSuccess>',
+    data: {
+      eng_name: 'doot',
+      fin_name: 'dööt',
+      sve_name: 'dååt',
+      skillLevelId: 3,
+      categoryId: 7,
+      courseId: 1,
+      id: 15
+    }
+  }
   let data
 
   beforeEach(() => {
@@ -17,9 +30,33 @@ describe('addObjective function', () => {
   it('returns a promise', () => {
     expect(typeof addObjective(data).then).toEqual('function')
   })
+
+  describe('dispatch', () => {
+    let dispatch
+    let withDispatch
+
+    beforeEach(() => {
+      dispatch = jest.fn()
+      withDispatch = asyncAction(addObjective, dispatch)
+    })
+
+    it('is called with correct action.', async () => {
+      await withDispatch(data)
+      expect(dispatch).toHaveBeenCalledWith({
+        type: 'OBJECTIVE_CREATE',
+        response: mockResponse
+      })
+    })
+  })
 })
 
 describe('removeObjective function', () => {
+  const mockResponse = {
+    message: '<removeObjectiveSuccess>',
+    data: {
+      id: 1
+    }
+  }
   let data
 
   beforeEach(() => {
@@ -30,5 +67,23 @@ describe('removeObjective function', () => {
 
   it('returns a promise', () => {
     expect(typeof removeObjective(data).then).toEqual('function')
+  })
+
+  describe('dispatch', () => {
+    let dispatch
+    let withDispatch
+
+    beforeEach(() => {
+      dispatch = jest.fn()
+      withDispatch = asyncAction(removeObjective, dispatch)
+    })
+
+    it('is called with correct action.', async () => {
+      await withDispatch(data)
+      expect(dispatch).toHaveBeenCalledWith({
+        type: 'OBJECTIVE_DELETE',
+        response: mockResponse
+      })
+    })
   })
 })
