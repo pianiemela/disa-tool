c3a25332f957a1343cfe1f2ae9b96fdfdeb82cc4
Salo Jemi S <jemi.salo@gmail.com>
2018-07-11 15:50:14 +0300

    Turned off operator aliases just in case.

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -3,7 +3,8 @@ const conf = require('../../conf-backend.js')
 
 const sequelize = new Sequelize(conf.DB_URL, {
   dialect: 'postgres',
-  logging: false
+  logging: false,
+  operatorsAliases: false
 })
 
 module.exports = {
