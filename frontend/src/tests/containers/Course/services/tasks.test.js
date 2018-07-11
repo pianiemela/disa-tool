8d16b531c1fd1deb238311c305b801e625621208
Salo Jemi S <jemi.salo@gmail.com>
2018-07-11 12:42:53 +0300

    Test removeTask returns a promise that resolves with correct action.

--- frontend/src/tests/containers/Course/services/tasks.test.js
+++ frontend/src/tests/containers/Course/services/tasks.test.js
@@ -6,10 +6,18 @@ import {
   addTypeToTask,
   removeTypeFromTask
 } from '../../../../containers/Course/services/tasks'
-import asyncAction from '../../../../utils/asyncAction'
+import { testService } from '../../../testUtils'
 
-describe('addTask function', () => {
-  const mockResponse = {
+testService({
+  function: addTask,
+  type: 'TASK_CREATE',
+  data: {
+    eng_name: 'doot',
+    fin_name: 'dööt',
+    sve_name: 'dååt',
+    courseId: 1
+  },
+  mockResponse: {
     message: '<addTaskSuccess>',
     data: {
       eng_name: 'doot',
@@ -19,36 +27,18 @@ describe('addTask function', () => {
       id: 2
     }
   }
-  let data
+})
 
-  beforeEach(() => {
-    data = {
-      eng_name: 'doot',
-      fin_name: 'dööt',
-      sve_name: 'dååt',
-      courseId: 1
+testService({
+  function: removeTask,
+  type: 'TASK_DELETE',
+  data: {
+    id: 3
+  },
+  mockResponse: {
+    message: '<removeTaskSuccess>',
+    data: {
+      id: 3
     }
-  })
-
-  it('returns a promise', () => {
-    expect(typeof addTask(data).then).toEqual('function')
-  })
-
-  describe('dispatch', () => {
-    let dispatch
-    let withDispatch
-
-    beforeEach(() => {
-      dispatch = jest.fn()
-      withDispatch = asyncAction(addTask, dispatch)
-    })
-
-    it('is called with correct action.', async () => {
-      await withDispatch(data)
-      expect(dispatch).toHaveBeenCalledWith({
-        type: 'TASK_CREATE',
-        response: mockResponse
-      })
-    })
-  })
+  }
 })
--- frontend/src/tests/testUtils.js
+++ frontend/src/tests/testUtils.js
@@ -1,4 +1,6 @@
-const findText = (text, wrapper) => {
+import asyncAction from '../utils/asyncAction'
+
+export const findText = (text, wrapper) => {
   let found = 0
   if (wrapper.text().includes(text)) found += 1
   wrapper.children().forEach((child) => {
@@ -7,6 +9,37 @@ const findText = (text, wrapper) => {
   return found
 }
 
-module.exports = {
-  findText
+/**
+ * 
+ * @param {*} options {
+ *  function,
+ *  data,
+ *  mockResponse,
+ *  type
+ * }
+ */
+export const testService = (options) => {
+  describe(`${options.function.name} function`, () => {
+    it('returns a promise', () => {
+      expect(typeof options.function(options.data).then).toEqual('function')
+    })
+  
+    describe('dispatch', () => {
+      let dispatch
+      let withDispatch
+  
+      beforeEach(() => {
+        dispatch = jest.fn()
+        withDispatch = asyncAction(options.function, dispatch)
+      })
+  
+      it('is called with correct action.', async () => {
+        await withDispatch(options.data)
+        expect(dispatch).toHaveBeenCalledWith({
+          type: options.type,
+          response: options.mockResponse
+        })
+      })
+    })
+  })
 }
