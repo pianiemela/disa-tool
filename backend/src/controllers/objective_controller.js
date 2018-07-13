8df8d5dc035347779d86e1954d677b02d2a44b7b
Salo Jemi S <jemi.salo@gmail.com>
2018-07-13 14:42:51 +0300

    Add messages to objective api responses.

--- backend/src/controllers/objective_controller.js
+++ backend/src/controllers/objective_controller.js
@@ -2,16 +2,39 @@ const router = require('express').Router()
 
 const objectiveService = require('../services/objective_service.js')
 
+const messages = {
+  create: {
+    success: {
+      eng: '"Oppimistavoite luotu onnistuneesti." englanniksi.',
+      fin: 'Oppimistavoite luotu onnistuneesti.',
+      sve: '"Oppimistavoite luotu onnistuneesti." ruotsiksi.'
+    }
+  },
+  delete: {
+    success: {
+      eng: '"Oppimistavoite poistettu onnistuneesti." englanniksi.',
+      fin: 'Oppimistavoite poistettu onnistuneesti.',
+      sve: '"Oppimistavoite poistettu onnistuneesti." ruotsiksi.'
+    }
+  }
+}
+
 router.post('/create', async (req, res) => {
   // TODO validate
-  const result = await objectiveService.create(req.body)
-  res.status(200).json(result)
+  const created = await objectiveService.create(req.body)
+  res.status(200).json({
+    message: messages.create.success[req.lang],
+    created
+  })
 })
 
 router.delete('/delete/:id', async (req, res) => {
   // TODO validate
-  const result = await objectiveService.delete(req.params.id)
-  res.status(200).json(result)
+  const deleted = await objectiveService.delete(req.params.id)
+  res.status(200).json({
+    message: messages.delete.success[req.lang],
+    deleted
+  })
 })
 
 module.exports = router
