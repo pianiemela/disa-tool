ef3fc5a00fe30c387407d49f88362c9a80cc0619
Salo Jemi S <jemi.salo@gmail.com>
2018-07-13 13:21:35 +0300

    Give sensible message in course_instance_controller.data

--- backend/src/controllers/course_instance_controller.js
+++ backend/src/controllers/course_instance_controller.js
@@ -1,11 +1,23 @@
 const router = require('express').Router()
 
 const courseInstanceService = require('../services/course_instance_service.js')
+const { validateLang } = require('../middleware/validate.js')
+
+const messages = {
+  data: {
+    success: {
+      eng: '"Kurssin data haettu onnistuneesti." englanniksi.',
+      fin: 'Kurssin data haettu onnistuneesti.',
+      sve: '"Kurssin data haettu onnistuneesti." ruotsiksi.'
+    }
+  }
+}
 
 router.get('/data/:courseInstanceId', async (req, res) => {
-  const data = await courseInstanceService.getCourseInstanceData(req.params.courseInstanceId, 'fin')
+  const lang = validateLang(req)
+  const data = await courseInstanceService.getCourseInstanceData(req.params.courseInstanceId, lang)
   res.status(200).json({
-    message: '<getDataForCourseSuccess>',
+    message: messages.data.success[lang],
     data
   })
 })
