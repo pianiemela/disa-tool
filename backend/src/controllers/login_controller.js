09944611dbe02b30ed115f3d51c415b1786dfd06
Salo Jemi S <jemi.salo@gmail.com>
2018-07-16 15:47:52 +0300

    Make login api route respond with user details or create a user and return details if not already in database.

--- backend/src/controllers/login_controller.js
+++ backend/src/controllers/login_controller.js
@@ -5,6 +5,23 @@ const https = require('https')
 
 const { Person } = require('../database/models.js')
 
+const messages = {
+  create: {
+    success: {
+      eng: '',
+      fin: '',
+      swe: ''
+    }
+  },
+  login: {
+    success: {
+      eng: '',
+      fin: '',
+      swe: ''
+    }
+  }
+}
+
 router.post('', async (req, res) => {
   const result = await axios.post(`https://${process.env.KURKI_URL}/login`, {
     username: req.body.username,
@@ -13,7 +30,9 @@ router.post('', async (req, res) => {
   {
     httpsAgent: new https.Agent({
       rejectUnauthorized: false
-    }) // This is not production-ready. This line makes axios accept any cert and exposes us to a man-in-the-middle attack.
+    }) // This is not production-ready!
+    // This line makes axios accept any cert and exposes us to a man-in-the-middle attack.
+    // Sending real AD credentials through this is a violation of some terms or conditions I'm sure.
   })
   if (result.data.error) {
     res.status(403).json({
@@ -21,7 +40,40 @@ router.post('', async (req, res) => {
     })
     return
   }
-  res.status(200).json(result.data)
+  const person = await Person.findOne({
+    where: {
+      studentnumber: result.data.student_number
+    },
+    attributes: ['id', 'name', 'studentnumber', 'role']
+  })
+  if (!person) {
+    const created = await Person.create({
+      studentnumber: result.data.student_number,
+      name: `${result.data.first_names.includes('*') ? (
+        result.data.first_names.split('*')[1].split(' ')[0]
+      ) : (
+        result.data.first_names.split(' ')[0]
+      )} ${result.data.last_name}`,
+      role: 'student'
+    })
+    const loggedIn = {
+      id: created.id,
+      studentnumber: created.studentnumber,
+      name: created.studentnumber,
+      role: created.role
+    }
+    res.status(200).json({
+      message: messages.create.success[req.lang],
+      logged_in: loggedIn,
+      created: true
+    })
+    return
+  }
+  res.status(200).json({
+    message: messages.login.success[req.lang],
+    logged_in: person.toJSON(),
+    created: false
+  })
 })
 
 module.exports = router
