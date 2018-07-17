c49060b819d278185ad675d46612a196398326f9
Salo Jemi S <jemi.salo@gmail.com>
2018-07-17 17:23:50 +0300

    Create multiplier field in CreateTypeForm.

--- frontend/src/containers/Course/components/types/CreateTypeForm.js
+++ frontend/src/containers/Course/components/types/CreateTypeForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { Button } from 'semantic-ui-react'
+import { Button, Form, Label, Input } from 'semantic-ui-react'
 import asyncAction from '../../../../utils/asyncAction'
 
 import { addType } from '../../services/types'
@@ -16,13 +16,16 @@ export class CreateTypeForm extends Component {
       eng_name: e.target.eng_name.value,
       fin_name: e.target.fin_name.value,
       swe_name: e.target.swe_name.value,
-      multiplier: 1.0
+      multiplier: Number(e.target.multiplier.value)
     })
   }
 
   render() {
     const contentPrompt = 'Lisää uusi tyyppi'
-    const label = 'nimi'
+    const label = {
+      name: 'nimi',
+      multiplier: 'kerroin'
+    }
     return (
       <div className="CreateTypeForm">
         <ModalForm
@@ -33,7 +36,11 @@ export class CreateTypeForm extends Component {
               <p>
                 {contentPrompt}.
               </p>
-              <MultilingualField field="name" fieldDisplay={label} />
+              <MultilingualField field="name" fieldDisplay={label.name} />
+              <Form.Field inline>
+                <Label>{label.multiplier}</Label>
+                <Input name="multiplier" type="number" min={0} max={1} step={0.01} />
+              </Form.Field>
               <Button type="submit" color="green">Tallenna</Button>
             </div>
           }
