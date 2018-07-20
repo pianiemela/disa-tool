f52a605216ce4a82cd21effa893fb524720900bf
Mikko Tiainen <mikko.o.tiainen@helsinki.fi>
2018-07-20 13:36:04 +0300

    created fake kurki app for dev logins

--- backend/src/controllers/login_controller.js
+++ backend/src/controllers/login_controller.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const { login, signJWT } = require('../services/login_service.js')
 
 router.post('', async (req, res) => {
-  if (!req.body.username.includes('tester')) {
+  if (process.env.NODE_ENV !== 'development' && !req.body.username.includes('tester')) {
     res.status(400).json({
       error: 'Stick to test users until security is patched.'
     })
