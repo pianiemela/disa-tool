0cc999650fc374db053da5c5ee68588b4cc1f46c
Salo Jemi S <jemi.salo@gmail.com>
2018-07-24 10:46:03 +0300

    Make add/remove services for objectives on tasks use the backend.

--- frontend/src/containers/EditTasks/services/tasks.js
+++ frontend/src/containers/EditTasks/services/tasks.js
@@ -1,27 +1,17 @@
 import { postJson, deleteCall } from '../../../utils/utils'
 
 export const addObjectiveToTask = data => new Promise((resolve) => {
-  const response = {
-    message: '<addObjectiveToTaskSuccess>',
-    data
-  }
-  const action = {
-    type: 'TASK_ADD_OBJECTIVE',
-    response
-  }
-  setTimeout(resolve, 100, action)
+  postJson('/tasks/objectives/attach', data).then(response => resolve({
+    type: 'TASK_ATTACH_OBJECTIVE',
+    response: response.data
+  }))
 })
 
 export const removeObjectiveFromTask = data => new Promise((resolve) => {
-  const response = {
-    message: '<removeObjectiveFromTaskSuccess>',
-    data
-  }
-  const action = {
-    type: 'TASK_REMOVE_OBJECTIVE',
-    response
-  }
-  setTimeout(resolve, 100, action)
+  postJson('/tasks/objectives/detach', data).then(response => resolve({
+    type: 'TASK_DETACH_OBJECTIVE',
+    response: response.data
+  }))
 })
 
 export const addTask = data => new Promise((resolve) => {
