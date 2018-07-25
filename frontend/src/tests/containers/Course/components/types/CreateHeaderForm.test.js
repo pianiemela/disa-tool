8999b88d9695db6de1a42c40372a1afaf57de019
Salo Jemi S <jemi.salo@gmail.com>
2018-07-25 13:43:20 +0300

    Test CreateHeaderForm

---
+++ frontend/src/tests/containers/Course/components/types/CreateHeaderForm.test.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { CreateHeaderForm } from '../../../../../containers/Course/components/types/CreateHeaderForm'
+import ModalForm from '../../../../../utils/components/ModalForm'
+
+const mockFn = () => {}
+
+describe('CreateHeaderForm component', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallow(<CreateHeaderForm
+      addHeader={mockFn}
+      courseId={1}
+    />)
+  })
+
+  it('renders.', () => {
+    expect(wrapper.find('.CreateHeaderForm').exists()).toEqual(true)
+  })
+
+  describe('ModalForm onSubmit', () => {
+    it('is a function.', () => {
+      expect(typeof wrapper.find(ModalForm).props().onSubmit).toEqual('function')
+    })
+  })
+})
