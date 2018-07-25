e9bd58a320cbfb7e5047872ec0e5fc8dfce81f0d
laimikko1 <mikko.s.laitinen@helsinki.fi>
2018-07-25 15:59:39 +0300

    Add loader to edit page when fetching data

--- frontend/src/containers/SelfAssesment/Userform/SelfAssesmentForm.js
+++ frontend/src/containers/SelfAssesment/Userform/SelfAssesmentForm.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Form, Grid, Button } from 'semantic-ui-react'
+import { Form, Grid, Button, Loader } from 'semantic-ui-react'
 import PropTypes from 'prop-types'
 import ObjectiveQuestionModule from './FormParts/QuestionModules/ObjectiveQuestionModule'
 import CategoryQuestionModule from './FormParts/QuestionModules/CategoryQuestionModule'
@@ -16,106 +16,98 @@ class SelfAssesmentForm extends React.Component {
   async componentDidMount() {
     if (!this.props.created) {
       const { selfAssesmentId } = this.props.match.params
-      this.props.dispatchGetSelfAssesmentAction(selfAssesmentId)
+      await this.props.dispatchGetSelfAssesmentAction(selfAssesmentId)
     }
   }
-
-  textArea = (label, placeholder, textFieldOn, checkbox) => (
-    (
-      <Grid.Column>
-        <Form.TextArea
-          disabled={!textFieldOn}
-          label={label}
-          placeholder={placeholder}
-        />
-        {checkbox}
-      </Grid.Column>
-    )
-  )
-
-  editForm = (formData, edit, bText, handleSubmit) => {
-    const { structure } = formData
-    const { displayCoursename, type, formInfo } = structure
-    const { openQ, questionHeaders, grade } = structure.headers
-    return (
-      <div>
-        <h2 style={{ textAlign: 'center' }}>{displayCoursename}</h2>
-
-        <SelfAssesmentInfo
-          textArea={textArea}
-          formData={formInfo}
-        />
-
-        {type === 'category' ?
-          <SelfAssesmentSection
-            headers={questionHeaders}
-            formData={structure.questionModules}
-            edit={edit}
-            textArea={textArea}
-            QuestionModule={CategoryQuestionModule}
-          />
-          :
-          <SelfAssesmentSection
-            headers={questionHeaders}
-            formData={structure.questionModules}
-            edit={edit}
-            textArea={textArea}
-            QuestionModule={ObjectiveQuestionModule}
-          />
-        }
-
-        <SelfAssesmentSection
-          headers={openQ}
-          formData={structure.openQuestions}
-          edit={edit}
-          textArea={textArea}
-          QuestionModule={OpenQuestionModule}
-          question
-        />
-
-        {type === 'category' ?
-
-          <SelfAssesmentSection
-            headers={grade}
-            formData={structure.finalGrade}
-            edit={edit}
-            textArea={textArea}
-            QuestionModule={CategoryQuestionModule}
-            final
-            headerType="grade"
-          />
-          :
-          null
-        }
-        <Button
-          positive
-          style={{ marginBottom: '25px' }}
-          onClick={handleSubmit}
-        >
-          {bText}
-        </Button>
-
-      </div>
-    )
-  }
-
-  renderEditableForm = () => {
-    if (this.props.edit) {
-      const { formData, edit, bText, handleSubmit } = this.props
-      return this.editForm(formData, edit, bText, handleSubmit)
-    }
-    return null
-  }
-
   render() {
-    const ceckprops = () => console.log(this.props.match.params, this.props)
+    console.log(`rerender`)
+    const textArea = (label, placeholder, textFieldOn, checkbox) => (
+      (
+        <Grid.Column>
+          <Form.TextArea
+            disabled={!textFieldOn}
+            label={label}
+            placeholder={placeholder}
+          />
+          {checkbox}
+        </Grid.Column>
+      )
+    )
+    const renderEditForm = () => {
+      const { formData, edit } = this.props
+      const { structure } = formData
+      const { displayCoursename, type, formInfo } = structure
+      const { openQ, questionHeaders, grade } = structure.headers
 
 
+      return (
+        <div>
+          <h2 style={{ textAlign: 'center' }}>{displayCoursename}</h2>
+
+          <SelfAssesmentInfo
+            textArea={textArea}
+            formData={formInfo}
+          />
+
+          {type === 'category' ?
+            <SelfAssesmentSection
+              headers={questionHeaders}
+              formData={structure.questionModules}
+              edit={edit}
+              textArea={textArea}
+              QuestionModule={CategoryQuestionModule}
+            />
+            :
+            <SelfAssesmentSection
+              headers={questionHeaders}
+              formData={structure.questionModules}
+              edit={edit}
+              textArea={textArea}
+              QuestionModule={ObjectiveQuestionModule}
+            />
+          }
+
+          <SelfAssesmentSection
+            headers={openQ}
+            formData={structure.openQuestions}
+            edit={edit}
+            textArea={textArea}
+            QuestionModule={OpenQuestionModule}
+            question
+          />
+
+          {type === 'category' ?
+
+            <SelfAssesmentSection
+              headers={grade}
+              formData={[structure.finalGrade]}
+              edit={edit}
+              textArea={textArea}
+              QuestionModule={CategoryQuestionModule}
+              final
+              headerType="grade"
+            />
+            :
+            null
+          }
+          <Button
+            positive
+            style={{ marginBottom: '25px' }}
+          // onClick={handleSubmit}
+          >
+            {/* {bText} */}
+          </Button>
+
+        </div>
+      )
+    }
     return (
       <div>
-        <p>moi</p>
-        {ceckprops()}
-        {/* {renderEditableForm()} */}
+        {this.props.edit && Object.keys(this.props.formData).length > 0 ?
+          renderEditForm()
+          :
+          <Loader active>Loading</Loader>
+        }
       </div >
     )
   }
