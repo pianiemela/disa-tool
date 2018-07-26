2a0388950f237ea31b72a42810d3a2ce7a161cf4
Salo Jemi S <jemi.salo@gmail.com>
2018-07-26 10:18:48 +0300

    Create login tokens for tests to use.

--- backend/src/tests/setupTests.js
+++ backend/src/tests/setupTests.js
@@ -1,2 +1,26 @@
+const jwt = require('jsonwebtoken')
 global.supertest = require('supertest')
 global.app = require('../app.js')
+const { Person } = require('../database/models.js')
+
+
+global.tokens = {
+  promise: Promise.all([
+    Person.findOne({
+      where: {
+        studentnumber: '012345678'
+      },
+      attributes: ['id', 'name', 'studentnumber', 'role']
+    }).then((student) => {
+      global.tokens.student = jwt.sign(student.toJSON(), process.env.SECRET)
+    }),
+    Person.findOne({
+      where: {
+        studentnumber: '012345679'
+      },
+      attributes: ['id', 'name', 'studentnumber', 'role']
+    }).then((teacher) => {
+      global.tokens.teacher = jwt.sign(teacher.toJSON(), process.env.SECRET)
+    })
+  ])
+}
