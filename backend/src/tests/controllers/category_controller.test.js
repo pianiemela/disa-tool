c6e8de00cb0a04d3ba3fe1a43d311a75545b89c3
Salo Jemi S <jemi.salo@gmail.com>
2018-07-27 14:28:47 +0300

    Test category_controller DELETE /:id.

--- backend/src/tests/controllers/category_controller.test.js
+++ backend/src/tests/controllers/category_controller.test.js
@@ -1,5 +1,11 @@
-const { testTeacherOnCoursePrivilege, testHeaders, testBody, testDatabaseSave } = require('../testUtils')
-const { Category } = require('../../database/models.js')
+const {
+  testTeacherOnCoursePrivilege,
+  testHeaders,
+  testBody,
+  testDatabaseSave,
+  testDatabaseDestroy
+} = require('../testUtils')
+const { Category, Objective } = require('../../database/models.js')
 
 describe('category_controller', () => {
   describe('POST /create', () => {
@@ -57,5 +63,68 @@ describe('category_controller', () => {
         disallowId: true
       }
     )
+  })
+
+  describe('DELETE /:id', () => {
+    const options = {
+      method: 'delete',
+      preamble: {
+        set: ['Authorization', `Bearer ${tokens.teacher}`]
+      }
+    }
+    let newId
+
+    beforeEach((done) => {
+      Category.create({
+        course_instance_id: 1,
+        eng_name: 'en',
+        fin_name: 'fn',
+        swe_name: 'sn'
+      }).then((result) => {
+        newId = result.get({ plain: true }).id
+        options.route = `/api/categories/${newId}`
+        done()
+      })
+    })
+
+    testTeacherOnCoursePrivilege(options)
+
+    testHeaders(options)
+
+    testBody(options, {
+      common: {
+        message: expect.any(String),
+        deleted: {
+          id: expect.any(Number),
+          tasks: []
+        }
+      }
+    })
+
+    describe('deletion cascades', () => {
+      const cascadeIds = {}
+      beforeEach((done) => {
+        Objective.create({
+          skill_level_id: 1,
+          course_instance_id: 1,
+          category_id: newId,
+          eng_name: 'eno',
+          fin_name: 'fno',
+          swe_name: 'sno'
+        }).then((result) => {
+          cascadeIds.objective = result.get({ plain: true }).id
+          done()
+        })
+      })
+      testDatabaseDestroy(options, Category, {
+        delay: 2000,
+        cascade: [
+          {
+            model: Objective,
+            getId: () => cascadeIds.objective
+          }
+        ]
+      })
+    })
   })
 })
