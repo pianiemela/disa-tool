58abcf31caad702f3ce9c164037f39460f28923e
Mikko Tiainen <mikko.o.tiainen@helsinki.fi>
2018-08-01 17:30:58 +0300

    logout working

--- frontend/src/redux/user.js
+++ frontend/src/redux/user.js
@@ -2,11 +2,13 @@
 export const userReducer = (state = {}, action) => {
   switch (action.type) {
     case 'USER_GET_SUCCESS':
-      return action.payload
+      return action.payload === '' ? {} : action.payload
     case 'USER_GET_FAILURE':
       return state
     case 'USER_LOGIN':
       return action.response.logged_in
+    case 'USER_LOGOUT':
+      return action.payload
     default:
       return state
   }
