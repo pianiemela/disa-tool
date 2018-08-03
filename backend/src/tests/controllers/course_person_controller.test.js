f20be9693ea12b5dbc2418e1ba087ec67c1b6fa9
Salo Jemi S <jemi.salo@gmail.com>
2018-08-03 10:27:29 +0300

    Test api routes for registering/unregistering.

---
+++ backend/src/tests/controllers/course_person_controller.test.js
@@ -0,0 +1,104 @@
+const {
+  testHeaders,
+  testStatusCode,
+  testBody,
+  testDatabaseSave,
+  testDatabaseDestroy
+} = require('../testUtils')
+const { CoursePerson } = require('../../database/models.js')
+
+describe('course_person_controller', () => {
+  describe('POST /register', () => {
+    const data = {
+      course_instance_id: 4
+    }
+    const options = {
+      route: '/api/course-persons/register',
+      method: 'post',
+      preamble: {
+        send: data,
+        set: ['Authorization', `Bearer ${tokens.student}`]
+      }
+    }
+
+    afterEach((done) => {
+      CoursePerson.destroy({
+        where: {
+          course_instance_id: data.course_instance_id
+        }
+      }).then(() => done())
+    })
+
+    testHeaders(options)
+
+    testStatusCode(options, 200)
+
+    testBody(options, {
+      common: {
+        message: expect.any(String),
+        created: {
+          course_instance_id: data.course_instance_id,
+          person_id: 421,
+          role: 'STUDENT'
+        }
+      }
+    })
+
+    testDatabaseSave(
+      options,
+      {
+        ...data,
+        id: expect.any(Number),
+        person_id: 421,
+        role: 'STUDENT'
+      },
+      CoursePerson
+    )
+  })
+
+  describe('POST /unregister', () => {
+    const data = {
+      course_instance_id: 4
+    }
+    const options = {
+      route: '/api/course-persons/unregister',
+      method: 'post',
+      preamble: {
+        send: data,
+        set: ['Authorization', `Bearer ${tokens.student}`]
+      }
+    }
+
+    beforeEach((done) => {
+      CoursePerson.create({
+        course_instance_id: data.course_instance_id,
+        person_id: 421,
+        role: 'STUDENT'
+      }).then(() => done())
+    })
+
+    testHeaders(options)
+
+    testStatusCode(options, 200)
+
+    testBody(options, {
+      common: {
+        message: expect.any(String),
+        deleted: {
+          id: expect.any(Number),
+          course_instance_id: data.course_instance_id,
+          person_id: 421,
+          role: 'STUDENT'
+        }
+      }
+    })
+
+    testDatabaseDestroy(
+      options,
+      CoursePerson,
+      {
+        delay: 2000
+      }
+    )
+  })
+})
