f739fc3e5c024d31628135cc0d63b1ee3e1d36e7
Salo Jemi S <jemi.salo@gmail.com>
2018-08-13 12:14:22 +0300

    Fix style and tests of SelectTaskDropdown changes.

--- frontend/src/containers/Course/components/tasks/SelectTaskDropdown.js
+++ frontend/src/containers/Course/components/tasks/SelectTaskDropdown.js
@@ -1,36 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Header, Dropdown, Button, Popup, Input } from 'semantic-ui-react'
+import { Header, Dropdown, Button, Popup, Input, Container } from 'semantic-ui-react'
 
 const SelectTaskDropdown = props => (
-  <Header as="h2">
-    <Dropdown
-      fluid
-      options={props.tasks.map(task => ({
-        key: task.id,
-        text: task.name,
-        value: task.id
-      }))}
-      placeholder="Valitse tehtävä tästä"
-      scrolling
-      search
-      selectOnBlur={false}
-      value={props.activeTask ? props.activeTask.id : null}
-      onChange={props.changeActive}
-    />
-    <Header.Subheader>Kerroin:
-      {props.activeTask ?
-        <Popup
-          content={<div><Input action type="number" value={props.activeTask.defaultMultiplier}><input /><Button content="päivitä" /></Input></div>}
-          trigger={<Button
-            basic
-            content={props.activeTask.defaultMultiplier}
-          />}
-          on="click"
-        />
-        : undefined}
-    </Header.Subheader>
-  </Header>
+  <Container>
+    <Header as="h2">
+      <Dropdown
+        fluid
+        options={props.tasks.map(task => ({
+          key: task.id,
+          text: task.name,
+          value: task.id
+        }))}
+        placeholder="Valitse tehtävä tästä"
+        scrolling
+        search
+        selectOnBlur={false}
+        value={props.activeTask ? props.activeTask.id : null}
+        onChange={props.changeActive}
+      />
+      <Header.Subheader>Kerroin:
+        {props.activeTask ?
+          <Popup
+            content={<div><Input action type="number" value={props.activeTask.defaultMultiplier}><input /><Button content="päivitä" /></Input></div>}
+            trigger={<Button
+              basic
+              content={props.activeTask.defaultMultiplier.toFixed(2)}
+            />}
+            on="click"
+          />
+          : undefined}
+      </Header.Subheader>
+    </Header>
+  </Container>
 )
 
 SelectTaskDropdown.propTypes = {
