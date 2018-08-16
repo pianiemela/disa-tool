43f18bc4ac974c7cef372b79fe72dc26c04fe117
Salo Jemi S <jemi.salo@gmail.com>
2018-08-16 11:58:29 +0300

    Create api route for creating a new grade.

--- backend/src/controllers/grade_controller.js
+++ backend/src/controllers/grade_controller.js
@@ -1,12 +1,19 @@
 const router = require('express').Router()
 
 const gradeService = require('../services/grade_service.js')
+const { errors } = require('../messages/global.js')
+const { checkPrivilege } = require('../services/privilege')
 
 const messages = {
   course: {
     eng: '"Kurssin arvosteluperusteet haettu onnistuneesti." englanniksi.',
     fin: 'Kurssin arvosteluperusteet haettu onnistuneesti.',
     swe: '"Kurssin arvosteluperusteet haettu onnistuneesti." ruotsiksi.'
+  },
+  create: {
+    eng: '"Arvosteluperuste tallennettu onnistuneesti." englanniksi.',
+    fin: 'Arvosteluperuste tallennettu onnistuneesti.',
+    swe: '"Arvosteluperuste tallennettu onnistuneesti." ruotsiksi.'
   }
 }
 
@@ -16,6 +23,40 @@ router.get('/course/:id', async (req, res) => {
     message: messages.course[req.lang],
     data
   })
+})
+
+router.post('/create', async (req, res) => {
+  try {
+    const toCreate = await gradeService.create.prepare(req.body)
+    if (!await checkPrivilege(req, [
+      {
+        key: 'teacher_on_course',
+        param: toCreate.dataValues.skill_level.course_instance_id
+      }
+    ])) {
+      res.status(403).json({
+        error: errors.privilege[req.lang]
+      })
+      return
+    }
+    await gradeService.create.execute(toCreate)
+    const created = gradeService.create.value(toCreate, req.lang)
+    res.status(200).json({
+      message: messages.create[req.lang],
+      created
+    })
+  } catch (e) {
+    if (process.env.NODE_ENV === 'development') {
+      res.status(500).json({
+        error: e
+      })
+    } else {
+      res.status(500).json({
+        error: errors.unexpected[req.lang]
+      })
+      console.log(e)
+    }
+  }
 })
 
 module.exports = router
--- backend/src/services/grade_service.js
+++ backend/src/services/grade_service.js
@@ -15,6 +15,40 @@ const getByCourse = async (id, lang) => {
   return result.map(grade => ({ ...grade.toJSON(), skill_level: undefined }))
 }
 
+const create = {
+  prepare: data => Grade.build(
+    {
+      eng_name: data.eng_name,
+      fin_name: data.fin_name,
+      swe_name: data.swe_name,
+      skill_level_id: data.skill_level_id,
+      needed_for_grade: data.needed_for_grade,
+      prerequisite: data.prerequisite
+    },
+    {
+      include: [
+        {
+          model: SkillLevel,
+          attributes: ['id', 'course_instance_id']
+        },
+        {
+          model: Grade,
+          attributes: ['id']
+        }
+      ]
+    }
+  ),
+  execute: instance => instance.save(),
+  value: (instance, lang) => {
+    const json = instance.toJSON()
+    return {
+      id: json.id,
+      name: json[`${lang}_name`]
+    }
+  }
+}
+
 module.exports = {
-  getByCourse
+  getByCourse,
+  create
 }
