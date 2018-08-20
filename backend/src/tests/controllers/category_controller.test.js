83d97d1ce48b0cfdaec24220c9762b7e90a9329a
Salo Jemi S <jemi.salo@gmail.com>
2018-08-20 11:37:05 +0300

    Create and test api routes for editing a category.

--- backend/src/tests/controllers/category_controller.test.js
+++ backend/src/tests/controllers/category_controller.test.js
@@ -130,4 +130,133 @@ describe('category_controller', () => {
       })
     })
   })
+
+  describe('GET /:id', () => {
+    const data = {
+      eng_name: 'en',
+      fin_name: 'fn',
+      swe_name: 'sn',
+      course_instance_id: 1
+    }
+    const options = {
+      route: '/api/categories',
+      method: 'get',
+      preamble: {}
+    }
+    const ids = {}
+
+    beforeAll((done) => {
+      Category.create(data).then((result) => {
+        ids.category = result.get({ plain: true }).id
+        options.route = `${options.route}/${ids.category}`
+        done()
+      }).catch(done)
+    })
+
+    testHeaders(options)
+
+    testStatusCode(options, 200)
+
+    testStatusCode({ ...options, route: '/api/categories/999999' }, 404)
+
+    testBody(options, {
+      common: {
+        message: expect.any(String),
+        data: {
+          ...data,
+          id: asymmetricMatcher(actual => actual === ids.category)
+        }
+      }
+    })
+  })
+
+  describe('PUT /:id', () => {
+    const data = {
+      eng_name: 'new en',
+      fin_name: 'new fn',
+      swe_name: 'new sn'
+    }
+    const options = {
+      route: '/api/categories',
+      method: 'put',
+      preamble: {
+        send: data,
+        set: ['Authorization', `Bearer ${tokens.teacher}`]
+      }
+    }
+    const ids = {}
+    const databaseExpectation = {}
+
+    beforeAll((done) => {
+      Category.create({
+        eng_name: 'en',
+        fin_name: 'fn',
+        swe_name: 'sn',
+        course_instance_id: 1
+      }).then((result) => {
+        ids.category = result.get({ plain: true }).id
+        options.route = `${options.route}/${ids.category}`
+        databaseExpectation.created_at = result.get({ plain: true }).created_at
+        done()
+      }).catch(done)
+    })
+
+    beforeEach((done) => {
+      Category.findById(ids.category).then(
+        instance => instance.update({
+          eng_name: 'en',
+          fin_name: 'fn',
+          swe_name: 'sn'
+        }).then(() => done()).catch(done)
+      ).catch(done)
+    })
+
+    testHeaders(options)
+
+    testTeacherOnCoursePrivilege(options)
+
+    testStatusCode({ ...options, route: '/api/categories/999999' }, 404)
+
+    testBody(options, {
+      common: {
+        message: expect.any(String),
+        edited: {
+          id: asymmetricMatcher(actual => actual === ids.category)
+        }
+      },
+      eng: {
+        edited: {
+          name: data.eng_name
+        }
+      },
+      fin: {
+        edited: {
+          name: data.fin_name
+        }
+      },
+      swe: {
+        edited: {
+          name: data.swe_name
+        }
+      }
+    })
+
+    testDatabaseSave(
+      options,
+      {
+        ...data,
+        id: asymmetricMatcher(actual => actual === ids.category),
+        course_instance_id: 1,
+        created_at: asymmetricMatcher(actual => !(
+          actual < databaseExpectation.created_at || actual > databaseExpectation.created_at
+        )),
+        updated_at: asymmetricMatcher(actual => actual > databaseExpectation.created_at)
+      },
+      Category,
+      {
+        pathToId: ['body', 'edited', 'id'],
+        includeTimestamps: false
+      }
+    )
+  })
 })
