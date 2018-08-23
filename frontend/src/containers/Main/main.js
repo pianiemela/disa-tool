c05ae24d12ca727171bf2a8f73b9ddacac054570
Salo Jemi S <jemi.salo@gmail.com>
2018-08-23 12:36:47 +0300

    Refactor main routes to redirect anonymous users back to the requested page after they've logged in.

--- frontend/src/containers/Main/main.js
+++ frontend/src/containers/Main/main.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { withRouter, Switch, Route } from 'react-router-dom'
@@ -23,12 +23,60 @@ class MathJaxProvider extends MathJax.Provider {
   hasNodes = true
 }
 
+class Keygen {
+  constructor() {
+    this.userKey = 0
+    this.anonKey = 0
+  }
+
+  user() {
+    const key = `user${this.userKey}`
+    this.userKey += 1
+    return key
+  }
+
+  anonymous() {
+    const key = `user${this.anonKey}`
+    this.anonKey += 1
+    return key
+  }
+}
+
+const keygen = new Keygen()
+
 class Main extends PureComponent {
   componentWillReceiveProps(newProps) {
     if (this.props.toast !== newProps.toast) {
       toast(newProps.toast.message, newProps.toast.options)
     }
   }
+
+  userRoutes = [
+    <Route exact path="/selfassesment/edit/:selfAssesmentId" render={({ match }) => <SelfAssesmentForm edit match={match} />} key={keygen.user()} />,
+    <Route exact path="/selfassesment/create/:courseInstanceId/:type" render={({ match }) => <SelfAssesmentForm edit new match={match} />} key={keygen.user()} />,
+    <Route exact path="/selfassesment/response/:selfAssesmentId" render={({ match }) => <SelfAssesmentForm edit={false} match={match} />} key={keygen.user()} />,
+    <Route path="/selfassesment/list/:selfAssesmentId" render={({ match }) => <SelfAssesmentListPage selfAssesmentId={Number(match.params.selfAssesmentId)} />} key={keygen.user()} />,
+    <Route exact path="/selfassesment/:courseId" component={SelfAssesmentPage} key={keygen.user()} />,
+    <Route path="/selfassesment" component={SelfAssesmentPage} key={keygen.user()} />,
+    <Route exact path="/user/course/:courseId" component={UserPage} key={keygen.user()} />,
+    <Route exact path="/user" component={UserPage} key={keygen.user()} />,
+    <Route path="/course/:id" component={CoursePage} key={keygen.user()} />,
+    <Route path="/tasks-responses/upload/:courseId" component={UploadResponsesPage} key={keygen.user()} />,
+    <Route exact path="/courses/create" component={CreateCoursePage} key={keygen.user()} />,
+    <Route path="/admin" component={AdminPage} key={keygen.user()} />,
+    <Route component={HomePage} key={keygen.user()} />
+  ]
+
+  anonymousRoutes = [
+    <Route path="/course/matrix/:id" component={MatrixPage} key={keygen.anonymous()} />,
+    <Route exact path="/courses" component={CourseListPage} key={keygen.anonymous()} />,
+    <Route exact path="/login" component={LoginPage} key={keygen.anonymous()} />,
+    <Route exact path="/" component={HomePage} key={keygen.anonymous()} />
+  ]
+
+  anonymousFallback = <Route
+    render={({ location }) => <LoginPage redirectTo={location.pathname} />}
+  />
 
   render() {
     return (
@@ -40,22 +88,8 @@ class Main extends PureComponent {
         />
         <MathJaxProvider>
           <Switch>
-            <Route exact path="/selfassesment/edit/:selfAssesmentId" render={({ match }) => <SelfAssesmentForm edit match={match} />} />
-            <Route exact path="/selfassesment/create/:courseInstanceId/:type" render={({ match }) => <SelfAssesmentForm edit new match={match} />} />
-            <Route exact path="/selfassesment/response/:selfAssesmentId" render={({ match }) => <SelfAssesmentForm edit={false} match={match} />} />
-            <Route path="/selfassesment/list/:selfAssesmentId" render={({ match }) => <SelfAssesmentListPage selfAssesmentId={Number(match.params.selfAssesmentId)} />} />
-            <Route exact path="/selfassesment/:courseId" component={SelfAssesmentPage} />
-            <Route path="/selfassesment" component={SelfAssesmentPage} />
-            <Route exact path="/user/course/:courseId" component={UserPage} />
-            <Route exact path="/user" component={UserPage} />
-            <Route path="/course/matrix/:id" component={MatrixPage} />
-            <Route path="/course/:id" component={CoursePage} />
-            <Route path="/tasks-responses/upload/:courseId" component={UploadResponsesPage} />
-            <Route exact path="/courses" component={CourseListPage} />
-            <Route exact path="/courses/create" component={CreateCoursePage} />
-            <Route exact path="/login" component={LoginPage} />
-            <Route path="/admin" component={AdminPage} />
-            <Route component={HomePage} />
+            {this.anonymousRoutes}
+            {this.props.user.role ? this.userRoutes : this.anonymousFallback}
           </Switch>
         </MathJaxProvider>
       </main>
@@ -67,11 +101,15 @@ Main.propTypes = {
   toast: PropTypes.shape({
     message: PropTypes.string,
     options: PropTypes.object
+  }).isRequired,
+  user: PropTypes.shape({
+    role: PropTypes.string
   }).isRequired
 }
 
 const mapStateToProps = state => ({
-  toast: state.toast
+  toast: state.toast,
+  user: state.user
 })
 
 export default withRouter(connect(mapStateToProps, null)(Main))
