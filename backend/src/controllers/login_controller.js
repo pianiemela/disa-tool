2a44aece1cbca6294600e9afff8a77e9b4df2bdf
Salo Jemi S <jemi.salo@gmail.com>
2018-08-31 17:45:23 +0300

    Refactor login controller/service to make the distinction between the two more consistent.

--- backend/src/controllers/login_controller.js
+++ backend/src/controllers/login_controller.js
@@ -1,23 +1,48 @@
 const router = require('express').Router()
 const { login, signJWT } = require('../services/login_service.js')
 
+const messages = {
+  create: {
+    eng: '"Uusi käyttäjä luotu onnistuneesti." englanniksi.',
+    fin: 'Uusi käyttäjä luotu onnistuneesti.',
+    swe: '"Uusi käyttäjä luotu onnistuneesti." ruotsiksi.'
+  },
+  login: {
+    eng: '"Sisäänkirjautuminen onnistui." englanniksi.',
+    fin: 'Sisäänkirjautuminen onnistui.',
+    swe: '"Sisäänkirjautuminen onnistui." ruotsiksi.'
+  }
+}
+
+const errors = {
+  wrong_creds: {
+    eng: '"Väärä salasana tai käyttäjänimi." englanniksi.',
+    fin: 'Väärä salasana tai käyttäjänimi.',
+    swe: '"Väärä salasana tai käyttäjänimi." ruotsiksi.'
+  }
+}
+
+const statuses = {
+  wrong_creds: 403
+}
+
 router.post('', async (req, res) => {
-  const result = await login(
-    {
-      username: req.body.username,
-      password: req.body.password
-    },
-    req.lang
-  )
+  const result = await login({
+    username: req.body.username,
+    password: req.body.password
+  })
   if (result.error) {
-    res.status(result.status).json({
-      error: result.error
+    res.status(statuses[result.error]).json({
+      error: errors[result.error][req.lang]
     })
     return
   }
   const token = signJWT(result.logged_in)
-  result.token = token
-  res.status(200).json(result)
+  res.status(200).json({
+    message: result.created ? messages.create[req.lang] : messages.login[req.lang],
+    logged_in: result.logged_in,
+    token
+  })
 })
 
 module.exports = router
