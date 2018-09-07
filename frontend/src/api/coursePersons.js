88e8104c10cd8e19a91d63d29c17fbd719e6e7f3
Salo Jemi S <jemi.salo@gmail.com>
2018-09-07 15:58:02 +0300

    Fix unregistering from a course.

--- frontend/src/api/coursePersons.js
+++ frontend/src/api/coursePersons.js
@@ -2,7 +2,7 @@ import { postJson, putJson } from '../utils/utils'
 
 export const register = data => postJson('/course-persons/register', data)
 
-export const unregister = data => putJson('/course-persons/unregister', data)
+export const unregister = data => postJson('/course-persons/unregister', data)
 
 export const changeCourseRole = data => putJson('/course-persons/course-role', data)
 
