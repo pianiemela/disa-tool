64302c14115b04965c2b38a6bf6a6edf70e760f1
Mikko Tiainen <mikko.o.tiainen@helsinki.fi>
2018-09-19 09:51:20 +0300

    migration for larger assessment instructions

--- backend/conf-backend.js
+++ backend/conf-backend.js
@@ -3,7 +3,14 @@ require('dotenv').config()
 const DB_URL = process.env.DB_URL
 const TEST_DB_URL = process.env.TEST_DB_URL
 
+const development = { url: DB_URL }
+const production = { url: DB_URL }
+const text = { url: TEST_DB_URL }
+
 module.exports = {
   DB_URL,
-  TEST_DB_URL
+  TEST_DB_URL,
+  development,
+  production,
+  text
 }
