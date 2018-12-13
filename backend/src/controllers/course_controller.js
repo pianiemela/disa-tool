47e7edb5675ae40108281bfcad91a54cee000b70
laimikko1 <mikko.s.laitinen@helsinki.fi>
2018-12-13 15:12:43 +0200

    Split userpage course data retrieval in two

--- backend/src/controllers/course_controller.js
+++ backend/src/controllers/course_controller.js
@@ -35,6 +35,48 @@ router.put('/instance/:courseId/toggle', async (req, res) => {
   res.status(200).json(instance)
 })
 
+
+router.post('/instance/:courseId/tasks', async (req, res) => {
+  try {
+    const { instance } = req.body
+    const { id } = instance
+    if (!instance) {
+      res.status(401).json({ error: 'Instance not found' })
+      return
+    }
+
+    const isTeacher = await checkPrivilege(req, [{
+      key: 'teacher_on_course',
+      param: id
+    }])
+    if (!isTeacher) {
+      res.status(403).json({ toast: errors.privilege.toast, error: errors.privilege[req.lang] })
+      return
+    }
+
+    const people = await personService.getPeopleOnCourse(id, instance.tasks.map(task => task.id))
+    instance.people = people
+    instance.tasks = instance.tasks.map(task => ({
+      ...task,
+      types: task.types.map(ttype => ({ ...ttype, name: `${ttype.type_header.name} ${ttype.name}` })
+      )
+    }))
+
+    res.status(200).json(instance)
+  } catch (e) {
+    if (process.env.NODE_ENV === 'development') {
+      res.status(500).json({
+        error: e.message
+      })
+    } else {
+      res.status(500).json({
+        error: errors.unexpected[req.lang]
+      })
+      logger.error(e)
+    }
+  }
+})
+
 router.get('/instance/:courseId', async (req, res) => {
   const { courseId } = req.params
   const user = await checkAuth(req)
@@ -43,21 +85,20 @@ router.get('/instance/:courseId', async (req, res) => {
     res.status(401).json({ error: 'You are not registered on this course' })
     return
   }
-  const courseRole = instance.people[0].course_person.role
-  if (courseRole !== 'TEACHER') {
+  // if (courseRole !== 'TEACHER') {
     const teachers = await personService.getCourseTeachers(courseId)
     instance.dataValues.people = teachers
-  } else {
-    const people = await personService.getPeopleOnCourse(courseId, instance.tasks.map(task => task.id))
-    instance.dataValues.people = people
-  }
-  instance.dataValues.courseRole = courseRole
+  // } else {
+  //   const people = await personService.getPeopleOnCourse(courseId, instance.tasks.map(task => task.id))
+  //   instance.dataValues.people = people
+  // }
+  instance.dataValues.courseRole = instance.people[0].course_person.role
 
-  instance.tasks = instance.tasks.map(task => ({
-    ...task,
-    types: task.types.map(ttype => ({ ...ttype, name: `${ttype.type_header.name} ${ttype.name}` })
-    )
-  }))
+  // instance.tasks = instance.tasks.map(task => ({
+  //   ...task,
+  //   types: task.types.map(ttype => ({ ...ttype, name: `${ttype.type_header.name} ${ttype.name}` })
+  //   )
+  // }))
   res.status(200).json(instance)
 })
 
