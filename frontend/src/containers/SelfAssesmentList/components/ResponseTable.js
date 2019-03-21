9960b280eda3505a2bd37c006006bca3942c6439
Rochet2 <tqbattlenet@gmail.com>
2019-03-21 11:06:57 +0200

    Implement sorting to self assessment listing table

--- frontend/src/containers/SelfAssesmentList/components/ResponseTable.js
+++ frontend/src/containers/SelfAssesmentList/components/ResponseTable.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { arrayOf, bool, func } from 'prop-types'
+import { arrayOf, bool, func, shape, number } from 'prop-types'
 import { connect } from 'react-redux'
 import { Button, Table } from 'semantic-ui-react'
 import ResponseAccordion from './ResponseAccordion'
@@ -15,58 +15,81 @@ const finalGradeMatches = (response) => {
   return true
 }
 
-const responseDifferences = (response) => {
-  if (!response.response.verification) return <Table.Cell>-</Table.Cell>
+export const calculateDifference = (response) => {
+  if (!response.response.verification) return null
   const { categoryVerifications } = response.response.verification
   const { length } = categoryVerifications
   const mean = categoryVerifications
     .reduce((acc, cur) => (acc + (cur.wantedGrade.difference / length)), 0)
   const sd = Math.sqrt((1 / (length - 1)) * categoryVerifications
     .reduce((acc, cur) => (acc + ((cur.wantedGrade.difference - mean) ** 2)), 0))
+  return { mean, sd }
+}
+
+const responseDifferences = (response) => {
+  const diff = calculateDifference(response)
+  if (!diff) return <Table.Cell collapsing>-</Table.Cell>
   return (
-    <Table.Cell positive={mean < 0} negative={mean > 0}>
-      {mean.toFixed(1)}, ±{sd.toFixed(1)}
+    <Table.Cell collapsing positive={diff.mean < 0} negative={diff.mean > 0}>
+      {diff.mean.toFixed(1)}, ±{diff.sd.toFixed(1)}
     </Table.Cell>
   )
 }
 
-const ResponseTable = props => (
-  <Table>
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell>selected</Table.HeaderCell>
-        <Table.HeaderCell>student</Table.HeaderCell>
-        <Table.HeaderCell>differences</Table.HeaderCell>
-        <Table.HeaderCell>last updated</Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
-    <Table.Body>
-      {props.responses.map(response => (
-        <Table.Row key={response.id}>
-          <Table.Cell>
-            <Button
-              basic
-              color="blue"
-              icon={props.selected ? 'minus' : 'plus'}
-              value={response.id}
-              onClick={props.selectResponse(response)}
-            />
-          </Table.Cell>
-          <Table.Cell negative={!finalGradeMatches(response)}>
-            <ResponseAccordion response={response} />
-          </Table.Cell>
-          {responseDifferences(response)}
-          <Table.Cell>{new Date(response.updated_at).toLocaleDateString()}</Table.Cell>
+const ResponseTable = (props) => {
+  const { headerindex: index, asc } = props.sortedHeader
+  const { onSort } = props
+  const direction = asc ? 'ascending' : 'descending'
+  return (
+    <Table sortable>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>selected</Table.HeaderCell>
+          <Table.HeaderCell sorted={index === 0 ? direction : null} onClick={() => onSort(0)}>
+            studentnumber
+          </Table.HeaderCell>
+          <Table.HeaderCell sorted={index === 1 ? direction : null} onClick={() => onSort(1)}>
+            student name
+          </Table.HeaderCell>
+          <Table.HeaderCell sorted={index === 2 ? direction : null} onClick={() => onSort(2)}>
+            differences
+          </Table.HeaderCell>
+          <Table.HeaderCell sorted={index === 3 ? direction : null} onClick={() => onSort(3)}>
+            last updated
+          </Table.HeaderCell>
         </Table.Row>
-    ))}
-    </Table.Body>
-  </Table>
-)
+      </Table.Header>
+      <Table.Body>
+        {props.responses.map(response => (
+          <Table.Row key={response.id}>
+            <Table.Cell collapsing>
+              <Button
+                basic
+                color="blue"
+                icon={props.selected ? 'minus' : 'plus'}
+                value={response.id}
+                onClick={props.selectResponse(response)}
+              />
+            </Table.Cell>
+            <Table.Cell collapsing>{response.person.studentnumber}</Table.Cell>
+            <Table.Cell negative={!finalGradeMatches(response)}>
+              <ResponseAccordion response={response} />
+            </Table.Cell>
+            {responseDifferences(response)}
+            <Table.Cell collapsing>{new Date(response.updated_at).toLocaleDateString()}</Table.Cell>
+          </Table.Row>
+      ))}
+      </Table.Body>
+    </Table>
+  )
+}
 
 ResponseTable.propTypes = {
   responses: arrayOf(responseProp).isRequired,
   selected: bool,
-  selectResponse: func.isRequired
+  selectResponse: func.isRequired,
+  onSort: func.isRequired,
+  sortedHeader: shape({ headerindex: number.isRequired, asc: bool.isRequired }).isRequired
 }
 
 ResponseTable.defaultProps = {
