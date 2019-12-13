795f4496ecb4b45c02a052dd69d783f76b9d78ba
Joona Heikkilä <21111572+cxcorp@users.noreply.github.com>
2019-12-13 16:53:49 +0200

    shibbo refresh hack

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,33 @@ import { LocalizeProvider } from 'react-localize-redux'
 import * as Sentry from '@sentry/browser'
 
 import { getUserAction } from './actions/actions'
+import { getUser } from './api/persons'
 import Nav from './containers/Nav/navbar'
 import Main from './containers/Main/main'
 import LocalizeWrapper from './containers/Localize/LocalizeWrapper'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.sessionAliveInterval = null
+  }
+
   componentDidMount() {
     this.props.getUserAction()
+
+    // keep shibboleth session alive
+    this.sessionAliveInterval = setInterval(async () => {
+      try {
+        await getUser()
+      } catch (e) {}
+    }, 60 * 1000)
+  }
+
+  componentWillUnmount() {
+    if (this.sessionAliveInterval !== null) {
+      clearInterval(this.sessionAliveInterval)
+      this.sessionAliveInterval = null
+    }
   }
 
   componentDidCatch(err) {
